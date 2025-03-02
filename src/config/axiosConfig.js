a1729f31eac867533518eab2419f4d2ae179fb1d
mariyamayoob <74500760+mariyamayoob@users.noreply.github.com>
2025-03-02 17:49:21 -0500

    Update axiosConfig.js

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,5 +1,5 @@
 // src/utils/axiosConfig.js
-import axios from 'axios';
+import axios from 'axios'; 
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api', // Use environment variable or fallback
